99638fb17c71ee747091fb54b8e404d69b868ce4
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-23 11:25:41 +0100

    React Rating Stars Done.

---
+++ react-rating-component/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RatingStars from './RatingStars';
+
+const App = () => {
+  return (
+    <>
+        <RatingStars />
+    </>
+  );
+}
+
+export default App;
--- react-rating-component/src/RatingStars.jsx
+++ react-rating-component/src/RatingStars.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
 
 const RatingStars = () => {
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+
   return (
-    <div>
-        RatingStars
+    <div className='app__rating-stars'>
+        {
+            [...Array(5)].map((star, i) => {
+              const ratingValue = i + 1;
+
+              return (
+                <label key={ i }>
+                  <input 
+                    type="radio" 
+                    name="rating" 
+                    value={ ratingValue } 
+                    onClick={ () => setRating(ratingValue) }
+                  />
+                  <FaStar 
+                    size={ 28 } 
+                    className='star' 
+                    onMouseEnter={ () => setHover(ratingValue) }
+                    onMouseLeave={ () => setHover(null) }
+                    color={ ratingValue <= (hover || rating) ? 'orangered' : '' }
+                  />
+                </label>
+              )
+            })
+        }
     </div>
   );
 }
